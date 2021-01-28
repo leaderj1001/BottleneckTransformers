0349947566fbbda65f17baa48c0cec4a03844e56
myeongjun <leaderj1001@gmail.com>
2021-01-28 16:54:27 +0900

    init:

---
+++ model.py
@@ -0,0 +1,150 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+def get_n_params(model):
+    pp=0
+    for p in list(model.parameters()):
+        nn=1
+        for s in list(p.size()):
+            nn = nn*s
+        pp += nn
+    return pp
+
+
+class MHSA(nn.Module):
+    def __init__(self, n_dims, width=14, height=14):
+        super(MHSA, self).__init__()
+
+        self.query = nn.Conv2d(n_dims, n_dims, kernel_size=1)
+        self.key = nn.Conv2d(n_dims, n_dims, kernel_size=1)
+        self.value = nn.Conv2d(n_dims, n_dims, kernel_size=1)
+
+        self.rel_h = nn.Parameter(torch.randn([1, n_dims, height, 1]), requires_grad=True)
+        self.rel_w = nn.Parameter(torch.randn([1, n_dims, 1, width]), requires_grad=True)
+
+        self.softmax = nn.Softmax(dim=-1)
+
+    def forward(self, x):
+        n_batch, C, width, height = x.size()
+        q = self.query(x).view(n_batch, C, -1)
+        k = self.key(x).view(n_batch, C, -1)
+        v = self.value(x).view(n_batch, C, -1)
+
+        content_content = torch.bmm(q.permute(0, 2, 1), k)
+
+        content_position = (self.rel_h + self.rel_w).view(1, C, -1).permute(0, 2, 1)
+        content_position = torch.matmul(content_position, q)
+
+        energy = content_content + content_position
+        attention = self.softmax(energy)
+
+        out = torch.bmm(v, attention.permute(0, 2, 1))
+        out = out.view(n_batch, C, width, height)
+
+        return out
+
+
+class Bottleneck(nn.Module):
+    expansion = 4
+
+    def __init__(self, in_planes, planes, stride=1, mhsa=False):
+        super(Bottleneck, self).__init__()
+
+        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(planes)
+        if not mhsa:
+            self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, padding=1, stride=stride, bias=False)
+        else:
+            if stride == 2:
+                self.conv2 = nn.Sequential(
+                    MHSA(planes, width=14, height=14),
+                    # MHSA(planes, width=8, height=8), # for CIFAR10
+                    nn.AvgPool2d(2, 2),
+                )
+            else:
+                self.conv2 = nn.Sequential(
+                    MHSA(planes, width=7, height=7),
+                    # MHSA(planes, width=4, height=4), # for CIFAR10
+                )
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.conv3 = nn.Conv2d(planes, self.expansion * planes, kernel_size=1, bias=False)
+        self.bn3 = nn.BatchNorm2d(self.expansion * planes)
+
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_planes != self.expansion*planes:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_planes, self.expansion*planes, kernel_size=1, stride=stride),
+                nn.BatchNorm2d(self.expansion*planes)
+            )
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = F.relu(self.bn2(self.conv2(out)))
+        out = self.bn3(self.conv3(out))
+        out += self.shortcut(x)
+        out = F.relu(out)
+        return out
+
+
+# reference
+# https://github.com/kuangliu/pytorch-cifar/blob/master/models/resnet.py
+class ResNet(nn.Module):
+    def __init__(self, block, num_blocks, num_classes=1000):
+        super(ResNet, self).__init__()
+        self.in_planes = 64
+
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias=False)
+        # self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1) # for ImageNet
+
+        self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
+        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
+        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2, mhsa=True)
+
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc = nn.Sequential(
+            nn.Dropout(0.3), # All architecture deeper than ResNet-200 dropout_rate: 0.2
+            nn.Linear(512 * block.expansion, num_classes)
+        )
+
+    def _make_layer(self, block, planes, num_blocks, stride=1, mhsa=False):
+        strides = [stride] + [1]*(num_blocks-1)
+        layers = []
+        for idx, stride in enumerate(strides):
+            layers.append(block(self.in_planes, planes, stride, mhsa))
+            self.in_planes = planes * block.expansion
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        out = self.relu(self.bn1(self.conv1(x)))
+        out = self.maxpool(out) # for ImageNet
+
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+
+        out = self.avgpool(out)
+        out = torch.flatten(out, 1)
+        out = self.fc(out)
+        return out
+
+
+def ResNet50():
+    return ResNet(Bottleneck, [3, 4, 6, 3], num_classes=1000)
+
+
+def main():
+    model = ResNet50()
+    x = torch.randn([2, 3, 224, 224])
+    print(model(x).size())
+    print(get_n_params(model))
+
+
+# if __name__ == '__main__':
+#     main()
